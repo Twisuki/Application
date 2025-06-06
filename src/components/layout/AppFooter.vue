8948d6942551324bfb3ab4849a3524ab5d3afb0d
Twisuki <Twisuki@outlook.com>
2025-06-06 23:24:58 +0800

    删了重构!

--- src/components/layout/AppFooter.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="footer">
-    <!-- 版权信息 - 灰色 -->
-    <div style="background: #adb5bd; height: 40px;"></div>
-  </div>
-</template>
--- src/components/layout/SideBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="sidebar">
-    <!-- 搜索框 - 青色 -->
-    <div class="search" style="background: #20c997; height: 50px; margin-bottom: 10px;"></div>
-
-    <!-- 分类列表 - 深绿 -->
-    <div class="categories" style="background: #2b8a3e; height: 300px;"></div>
-  </div>
-</template>
