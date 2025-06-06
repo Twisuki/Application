14ab2803ba1f45eefd09814dd03f9e5b4b274ec2
Twisuki <Twisuki@outlook.com>
2025-06-06 23:19:35 +0800

    ???

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,28 @@
-// src/router/index.js
-
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Docs from '@/views/Docs.vue'
+import DocDetail from '@/views/DocDetail.vue' // 必须导入
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-    meta: {
-      title: '首页'
-    }
+    meta: { title: '首页' }
   },
   {
     path: '/docs',
     name: 'Docs',
     component: Docs,
-    meta: {
-      title: '文档中心'
-    }
+    meta: { title: '文档中心' }
   },
   {
     path: '/docs/:id',
     name: 'DocDetail',
-    component: DocDetail,
-    meta: {
-      title: '文档详情'
-    },
+    component: DocDetail, // 使用已导入的组件
+    meta: { title: '文档详情' },
     props: true
   },
-  // 404 页面
   {
     path: '/:pathMatch(.*)*',
     redirect: '/'
@@ -39,17 +31,13 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    return savedPosition || { top: 0 }
-  }
+  routes
 })
 
-// 动态修改页面标题
 router.beforeEach((to) => {
-  if (to.meta.title) {
-    document.title = `${to.meta.title} | 我的文档站`
-  }
+  document.title = to.meta.title
+    ? `${to.meta.title} | 我的文档站`
+    : '我的文档站'
 })
 
 export default router
