e5932fe4ea86bbbc81163d84a1688fc693070f70
Twisuki <Twisuki@outlook.com>
2025-06-12 15:31:17 +0800

    侧边栏展开

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
 
+// 侧边栏打开/折叠
+import { ref } from "vue";
+
+const isSidebar = ref(true)
+
+function sidebarToggle() {
+  isSidebar.value = !isSidebar.value;
+}
 </script>
 
 <template>
-  <div id="sidebar">
+  <div id="sidebar" :class="{'active': isSidebar}">
+    <div id="sidebar-title">
+
+    </div>
     <div id="sidebar-container">
+      <div class="sidebar-item-1">
+        <div class="sidebar-item-2">
+          <div class="sidebar-item-3">
 
+          </div>
+        </div>
+      </div>
     </div>
-    <div id="sidebar-toggle">
 
-    </div>
+  </div>
+  <div id="sidebar-toggle" @click="sidebarToggle" :class="{'active': isSidebar}">
+    <span></span>
   </div>
 </template>
 
@@ -22,6 +40,11 @@
   left: 0;
   background-color: var(--sidebar-bgc);
   z-index: var(--sidebar-z-index);
+  display: none;
+}
+
+#sidebar.active {
+  display: block;
 }
 
 #sidebar::before {
@@ -29,8 +52,66 @@
   width: calc(100% - var(--sidebar-width));
   height: calc(100% - var(--navbar-height));
   position: fixed;
+  display: block;
   top: var(--navbar-height);
   right: 0;
   background-color: var(--sidebar-backdrop-bgc);
+}
+
+#sidebar-toggle {
+  width: 2rem;
+  height: calc(100% - var(--navbar-height));
+  position: fixed;
+  top: var(--navbar-height);
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: var(--sidebar-z-index);
+}
+
+#sidebar-toggle.active {
+  left: var(--sidebar-width);
+}
+
+#sidebar-toggle:hover {
+  background-color: color-mix(in srgb, var(--text-dark-gray), transparent 20%);
+}
+
+#sidebar-toggle span {
+  width: 1rem;
+  height: 1rem;
+  display: block;
+  position: relative;
+  border-radius: 2px;
+  border-top: 4px solid var(--text-dark);
+  border-right: 4px solid var(--text-dark);
+  rotate: 45deg;
+  transition: border 0.2s ease;
+}
+
+#sidebar-toggle.active span {
+  border-top: none;
+  border-right: none;
+  border-bottom: 4px solid var(--text-dark);
+  border-left: 4px solid var(--text-dark);
+}
+
+/* 超宽屏幕 */
+@media only screen and (min-width: 2560px) {
+  #sidebar {
+    width: var(--sidebar-width);
+    height: calc(100% - var(--navbar-height));
+    position: fixed;
+    top: var(--navbar-height);
+    left: calc((100% - 1280px) / 2 - var(--sidebar-width) - 2rem);
+    background-color: var(--sidebar-bgc);
+    box-shadow: 0 -0.5rem 0.25rem 0.5rem #00000080;
+    z-index: var(--sidebar-z-index);
+  }
+
+  #sidebar::before {
+    display: none;
+  }
 }
 </style>
