a251fcf930b896b3b6534619384503534451811b
Twisuki <Twisuki@outlook.com>
2025-06-14 13:24:26 +0800

    添加页脚

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,6 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div class="footer">
-    <!-- 版权信息 - 灰色 -->
-    <div style="background: #adb5bd; height: 40px;"></div>
+  <div id="footer">
+    <a href="https://robotics.hnu.edu.cn/" class="underline">湖南大学机器人学院</a> &copy; 2024 <a href="https://github.com/TZLakeStudio" class="underline HYTMR">桃子湖工作站</a>
   </div>
 </template>
+
+<style scoped>
+#footer {
+  width: 100%;
+  height: 2rem;
+  padding: 0;
+  margin: 0;
+  background-color: var(--footer-bgc);
+  text-align: center;
+  line-height: 2rem;
+}
+
+#footer a {
+  color: var(--text-dark-gray);
+}
+
+#footer a::after {
+  background-color: var(--text-dark-gray);
+  bottom: -2px;
+}
+</style>
